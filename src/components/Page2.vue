eb4a4a576a7b5dc7e8d937aed0ee8fd4a0c34cf0
1464572 <hovington234@gmail.com>
2021-03-20 15:08:53 -0400

    Push avant homework maison

---
+++ src/components/Page2.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Voici page 2</p>
+</template>
+<script>
+
+
+export default{
+    name:'page2'
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/tailwind.css'
+import VueRouter from 'vue-router'
+import Routes from './Routes.js'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history',
+})
+
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
