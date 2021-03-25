b73d34025bbf73c6e57b38a77f2aac886680fd0e
1464572 <hovington234@gmail.com>
2021-03-24 20:40:05 -0400

    Started the programmation

---
+++ src/components/Controls.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quos magnam consequatur a fugit, reprehenderit omnis temporibus architecto deleniti, perspiciatis voluptates, ipsam fugiat ex itaque earum velit aperiam officia eum.</p>
+</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {}
+}
+</script>
+
+<style>
+
+</style>
